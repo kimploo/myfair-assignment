b16e49f1211414e50b06b1400d99ea54cc0ed0d3
Hong Shik Branden Kim <kimploo@gmail.com>
2024-10-27 22:17:54 +0900

    feat: 개발 초기 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
+import localFont from "next/font/local";
 import React from "react";
+
 import LayoutRecoil from "./layout.recoil";
+import { GlobalStyles } from "../util/GlobalStyles";
 
 export const metadata = {
   title: "myfair front pre-course",
   description: "todolist",
 };
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "./fonts/Pretendard-Regular.woff",
+      weight: "400",
+    },
+    {
+      path: "./fonts/Pretendard-Bold.woff",
+      weight: "700",
+    },
+  ],
+  variable: "--font-pretendard",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +30,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <GlobalStyles></GlobalStyles>
+      <body className={`${pretendard.variable} font-pretendard`}>
         <LayoutRecoil>{children}</LayoutRecoil>
       </body>
     </html>
--- src/components/pages/TodoUserListPage.tsx
+++ src/components/pages/TodoUserListPage.tsx
@@ -1,15 +1,24 @@
 "use client";
-import React from "react";
-import styled from "@emotion/styled";
 
-const Container = styled.div``;
+import styled from "@emotion/styled";
+import React from "react";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  font-weight: 600;
+`;
 
 interface Props {}
 
 const TodoUserListPage = ({}: Props) => {
   return (
     <Container>
-      <div>todo</div>
+      <Heading>TODO Application</Heading>
     </Container>
   );
 };
