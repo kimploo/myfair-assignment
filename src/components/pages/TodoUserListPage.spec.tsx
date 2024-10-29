2144747ccd71aebc9c561bbeae0b68def0685337
Hong Shik Branden Kim <kimploo@gmail.com>
2024-10-30 04:30:59 +0900

    feat: 렌더링 테스트 추가

--- src/components/pages/TodoUserListPage.spec.tsx
+++ src/components/pages/TodoUserListPage.spec.tsx
@@ -1,29 +1,31 @@
-// import { render } from "@testing-library/react";
-// import { RecoilRoot, RecoilState } from "recoil";
+import { render, screen } from "@testing-library/react";
+import { RecoilRoot, RecoilState } from "recoil";
 
-// import { todoState } from "../../features/todo/state/todo.atom";
-// import { dummyTodos } from "../../util/dummyData";
+import LayoutTheme from "../../app/layout.theme";
+import { todoState } from "../../features/todo/state/todo.atom";
+import { dummyTodos } from "../../util/dummyData";
 
-// import TodoUserListPage from "./TodoUserListPage";
+import TodoUserListPage from "./TodoUserListPage";
 
-// // Define the initial state type, replace `YourAtomType` with the actual type of your atom.
-// const initializeState = ({
-//   set,
-// }: {
-//   set: <T>(recoilState: RecoilState<T>, value: T) => void;
-// }) => {
-//   set(todoState, dummyTodos);
-// };
+const initializeState = ({
+  set,
+}: {
+  set: <T>(recoilState: RecoilState<T>, value: T) => void;
+}) => {
+  set(todoState, dummyTodos);
+};
 
-// describe("TodoUserListPage 테스트", () => {
-//   it("제목 렌더링", () => {
-//     const page = render(
-//       <RecoilRoot initializeState={initializeState}>
-//         <TodoUserListPage />
-//       </RecoilRoot>,
-//     );
+describe("TodoUserListPage 테스트", () => {
+  it("제목 렌더링", () => {
+    render(
+      <LayoutTheme>
+        <RecoilRoot initializeState={initializeState}>
+          <TodoUserListPage></TodoUserListPage>
+        </RecoilRoot>
+      </LayoutTheme>,
+    );
 
-//     const h1 = page.getByText("TODO App");
-//     expect(h1).toBeVisible();
-//   });
-// });
+    const h1 = screen.getByText("TODO App");
+    expect(h1).toBeVisible();
+  });
+});
